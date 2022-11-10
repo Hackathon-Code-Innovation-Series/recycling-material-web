5707d0c17aed2ec27a1615ba25b3567b1a6a1fb4
Edison Salinas <salinasedisoncruz@gmail.com>
2022-11-10 09:45:06 -0500

    Javascript del mensaje

---
+++ assets/js/cabecera/mensaje/mensaje.js
@@ -0,0 +1,11 @@
+const typed=new Typed('.typed',{
+    strings:['Reduce', 'Reutiliza', 'Recicla'],
+    typeSpeed: 75, //Velocidad en milisegundos para colocar cada palabras
+    startDelay: 300,
+    backSpeed: 75,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: false,
+}
+);
